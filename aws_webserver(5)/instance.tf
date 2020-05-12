1b92ff01e69cd177490f6e05bc0baca0b0756911
giddy87 <55620120+giddy87@users.noreply.github.com>
2020-05-12 23:49:49 +0100

    Add files via upload

---
+++ aws_webserver(5)/instance.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "web-instance" {
+count = var.instance_count
+ami = "ami-0eb89db7593b5d434"
+instance_type = "t2.micro"
+key_name = aws_key_pair.ec2_key.key_name
+user_data = file("install_apache.sh")
+vpc_security_group_ids = [aws_security_group.web_server.id]
+
+
+tags = {
+Name = "web-server-${count.index +1}" 
+}
+
+}
