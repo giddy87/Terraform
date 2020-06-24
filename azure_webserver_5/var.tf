512baeb2eadd2fceec982e1091bca42cd5bc1e71
giddy <idahosagideon@gmail.com>
2020-06-24 11:09:58 +0000

    added public ip address

--- azure_webserver_5/var.tf
+++ azure_webserver_5/var.tf
@@ -2,6 +2,6 @@ variable "Tenant_id" {}
 variable "Client_id" {}
 variable "Client_secret" {}
 variable "Location" {}
-variable "instance_count" {default = "5"}
+variable "instance_count" {default = "2"}
 variable "password" {}
-variable "nic_count" {default = "5"}
+variable "nic_count" {default = "2"}
